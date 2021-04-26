866acffdc2732bc556169de7537c47dae22a4a9f
Anton Weiss <anton@otomato.io>
2021-04-26 10:18:44 +0300

    working code

--- controllers/plover_controller.go
+++ controllers/plover_controller.go
@@ -19,67 +19,25 @@ package controllers
 import (
 	"context"
 
+	"github.com/go-logr/logr"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/controller"
-	"sigs.k8s.io/controller-runtime/pkg/handler"
-	"sigs.k8s.io/controller-runtime/pkg/manager"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	canarianv1alpha1 "canarian.io/plover/api/v1alpha1"
 )
 
-// +kubebuilder:rbac
-type PloverController struct {
-	client.Client
-	scheme *runtime.Scheme
-}
-
-func Add(mgr manager.Manager) error {
-	// Create a new Controller
-	c, err := controller.New("plover-controller", mgr,
-		controller.Options{Reconciler: &PloverController{
-			Client: mgr.GetClient(),
-			scheme: mgr.GetScheme(),
-		}})
-	if err != nil {
-		return err
-	}
-
-	// Watch for changes to Plover
-	err = c.Watch(
-		&source.Kind{Type: &canarianv1alpha1.Plover{}},
-		&handler.EnqueueRequestForObject{})
-	if err != nil {
-		return err
-	}
-
-	// Watch for changes to Deployments created by a ContainerSet and trigger a Reconcile for the owner
-	err = c.Watch(
-		&source.Kind{Type: &v1.Pod{}},
-		&handler.EnqueueRequestForObject{})
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
 // PloverReconciler reconciles a Plover object
-// type PloverReconciler struct {
-// 	client.Client
-// 	Log    logr.Logger
-// 	Scheme *runtime.Scheme
-// }
+type PloverReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
 
 // +kubebuilder:rbac:groups=canarian.canarian.io,resources=plovers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=canarian.canarian.io,resources=plovers/status,verbs=get;update;patch
 
-var _ reconcile.Reconciler = &PloverController{}
-
-func (r *ReconcilePlover) Reconcile(req ctrl.Request) (ctrl.Result, error) {
+func (r *PloverReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
 	log := r.Log.WithValues("plover", req.NamespacedName)
 
@@ -97,8 +55,8 @@ func (r *ReconcilePlover) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	return ctrl.Result{}, nil
 }
 
-// func (r *PloverReconciler) SetupWithManager(mgr ctrl.Manager) error {
-// 	return ctrl.NewControllerManagedBy(mgr).
-// 		For(&canarianv1alpha1.Plover{}).
-// 		Complete(r)
-// }
+func (r *PloverReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&canarianv1alpha1.Plover{}).
+		Complete(r)
+}
